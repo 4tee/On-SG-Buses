5087b6ddbb516d467f059db714efe0b570b740a3
ts <mmcalendar2012@gmail.com>
2015-05-19 17:23:31 +0800

    Remove the button and also change SQL statement to retrieve unique bus numbers.

--- app/src/main/java/net/felixmyanmar/onsgbuses/MainActivity.java
+++ app/src/main/java/net/felixmyanmar/onsgbuses/MainActivity.java
@@ -2,37 +2,41 @@ package net.felixmyanmar.onsgbuses;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends Activity {
+
+    private MyCoolDatabase myCoolDatabase;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<String> myDataset;
+
+    @InjectView(R.id.cool_recycler_view)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        myCoolDatabase = new MyCoolDatabase(this);
+        myDataset = myCoolDatabase.getAllBusServices();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        ButterKnife.inject(this);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        recyclerView.setHasFixedSize(true);
 
-        return super.onOptionsItemSelected(item);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new CoolRecycleViewAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
