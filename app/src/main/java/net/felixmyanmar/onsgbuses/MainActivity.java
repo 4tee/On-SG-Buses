0baeea73c352b0b1a7e46c58ce2c8723f758aa59
ts <mmcalendar2012@gmail.com>
2015-05-21 15:28:39 +0800

    Added filter on search

--- app/src/main/java/net/felixmyanmar/onsgbuses/MainActivity.java
+++ app/src/main/java/net/felixmyanmar/onsgbuses/MainActivity.java
@@ -9,7 +9,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.transition.Fade;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,8 +25,7 @@ import butterknife.InjectView;
 import butterknife.OnItemClick;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     //    private RecyclerView.LayoutManager mLayoutManager;
 //    private RecyclerView.Adapter mAdapter;
@@ -64,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 R.layout.bus_service_item, allBusNos);
         listView.setAdapter(adapter);
+        listView.setTextFilterEnabled(true);
 
 //        recyclerView.setHasFixedSize(true);
 //
@@ -85,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
             SearchManager searchManager =
                     (SearchManager) getSystemService(Context.SEARCH_SERVICE);
             SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+            searchView.setQueryHint("Search Bus");
+            searchView.setOnQueryTextListener(this);
 
             ComponentName cn = new ComponentName(this, SearchResultsActivity.class);
             searchView.setSearchableInfo(searchManager.getSearchableInfo(cn));
@@ -103,5 +107,38 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return false;
         }
+    }
+
+    /**
+     * Called when the user submits the query. This could be due to a key press on the
+     * keyboard or due to pressing a submit button.
+     * The listener can override the standard behavior by returning true
+     * to indicate that it has handled the submit request. Otherwise return false to
+     * let the SearchView handle the submission by launching any associated intent.
+     *
+     * @param query the query text that is to be submitted
+     * @return true if the query has been handled by the listener, false to let the
+     * SearchView perform the default action.
+     */
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    /**
+     * Called when the query text is changed by the user.
+     *
+     * @param newText the new content of the query text field.
+     * @return false if the SearchView should perform the default action of showing any
+     * suggestions if available, true if the action was handled by the listener.
+     */
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.d("ts", "here");
+        if (TextUtils.isEmpty(newText))
+            listView.clearTextFilter();
+        else
+            listView.setFilterText(newText);
+        return true;
     }
 }
